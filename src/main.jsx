1c38cb0b729fa508c7c591a03dd1bb3995ab6a73
Fernando Tona <105774270+fernandotonacoder@users.noreply.github.com>
2025-12-15 01:25:36 +0000

    fix: add page not found handling

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,16 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 
+const redirect = sessionStorage.getItem("redirect");
+if (redirect) {
+  sessionStorage.removeItem("redirect");
+  window.history.replaceState(null, "", redirect);
+}
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter basename="/chrisert">
       <App />
     </BrowserRouter>
-  </StrictMode>
-);
+  </StrictMode>,
+)
