1181d3a07655e37497a236035406741fbe69a7f6
Fernando Tona <105774270+fernandotonacoder@users.noreply.github.com>
2025-12-21 03:56:36 +0000

    first cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { Navbar01 } from "@/components/ui/shadcn-io/navbar-01";
 import CustomFooter from "./components/ui/customFooter";
-// import PriceSimulatorPage from "./pages/PriceSimulatorPage";
-// import QuotePrintPage from "./pages/QuotePrintPage";
 import HomePage from "./pages/HomePage";
 import ServicesPage from "./pages/ServicesPage";
 import PortfolioPage from "./pages/PortfolioPage";
@@ -22,16 +20,7 @@ function App() {
           <Route path="/servicos" element={<ServicesPage />} />
           <Route path="/portfolio" element={<PortfolioPage />} />
           <Route path="/sobre" element={<AboutPage />} />
-          {/* <Route
-            path="/simulador"
-            element={
-              <div className="container mx-auto px-4 py-10">
-                <PriceSimulatorPage />
-              </div>
-            }
-          /> */}
           <Route path="/contactos" element={<ContactPage />} />
-          {/* <Route path="/orcamento" element={<QuotePrintPage />} /> */}
         </Routes>
       </main>
       <div className="print:hidden">
